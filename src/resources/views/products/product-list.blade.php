ff9770fc150d1dc230e1864cb064e938c00b551d
suzuki chiemi <thankschimail@gmail.com>
2025-01-22 12:01:52 +0900

    product-list.css完成

--- src/resources/views/products/product-list.blade.php
+++ src/resources/views/products/product-list.blade.php
@@ -7,21 +7,27 @@
 @section('content')
 <div class="container">
     <!-- タブ切り替え -->
-    <div class="tabs">
-        <a href="{{ route('products.index', ['tab' => 'recommend']) }}" class="{{ $tab === 'recommend' ? 'active' : '' }}">おすすめ</a>
-        <a href="{{ route('products.index', ['tab' => 'mylist']) }}" class="{{ $tab === 'mylist' ? 'active' : '' }}">マイリスト</a>
+    <div class="tabs-container">
+        <div class="tabs">
+            <a href="{{ route('products.index', ['tab' => 'recommend']) }}"
+                class="tabs__link {{ $tab === 'recommend' ? 'tabs__link--active' : '' }}">おすすめ</a>
+            <a href="{{ route('products.index', ['tab' => 'mylist']) }}"
+                class="tabs__link {{ $tab === 'mylist' ? 'tabs__link--active' : '' }}">マイリスト</a>
+        </div>
     </div>
+
 
     <!-- 商品リスト -->
     <div class="product-list">
         @forelse ($products as $product)
-        <div class="product-item">
-            <img src="{{ $product->image_path }}" alt="商品画像" class="product-item__image">
-            <p class="product-item__name">{{ $product->name }}</p>
+        <div class="product-list__item">
+            <img src="{{ $product->image_path }}" alt="商品画像" class="product-list__image">
+            <p class="product-list__name">{{ $product->name }}</p>
         </div>
         @empty
-        <p>商品がありません。</p>
+        <p class="product-list__empty">商品がありません。</p>
         @endforelse
     </div>
 </div>
+
 @endsection
