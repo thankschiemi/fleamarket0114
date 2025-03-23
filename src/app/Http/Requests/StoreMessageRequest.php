b2ce42e59d305e6d584826150c4a6b87df7419fc
suzuki chiemi <thankschimail@gmail.com>
2025-03-23 18:32:34 +0900

    出品者用取引画面のバリテーションとcss調整完成

--- src/app/Http/Requests/StoreMessageRequest.php
+++ src/app/Http/Requests/StoreMessageRequest.php
@@ -8,13 +8,13 @@ class StoreMessageRequest extends FormRequest
 {
     public function authorize()
     {
-        return true; // 認可チェック済みなのでtrueでOK
+        return true;
     }
 
     public function rules()
     {
         return [
-            'content' => 'required_without:image|string|max:400',
+            'message' => 'required_without:image|string|max:400',
             'image' => 'nullable|file|mimes:jpeg,png',
         ];
     }
@@ -22,8 +22,8 @@ class StoreMessageRequest extends FormRequest
     public function messages()
     {
         return [
-            'content.required_without' => '本文を入力してください',
-            'content.max' => '本文は400文字以内で入力してください',
+            'message.required_without' => '本文を入力してください',
+            'message.max' => '本文は400文字以内で入力してください',
             'image.mimes' => '「.png」または「.jpeg」形式でアップロードしてください',
         ];
     }
